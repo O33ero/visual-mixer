4bf8154490367461ad92d559fd978857ef12588e
O33ero <58762519+O33ero@users.noreply.github.com>
2024-05-18 16:58:41 +0300

    multiprocess of vfe_test

--- vfe_test.py
+++ vfe_test.py
@@ -1,4 +1,5 @@
 import os
+from concurrent.futures import ProcessPoolExecutor, wait
 
 import torch
 import torchvision
@@ -12,11 +13,11 @@ from tqdm import *
 
 from VisualMixer import AttentionBlock, rank, vfe
 
-VFE_BOX_SIZE = 28
-IMAGE_BOX_W = 448
-IMAGE_BOX_H = 448
+VFE_BOX_SIZE = 14
+IMAGE_BOX_W = 224
+IMAGE_BOX_H = 224
 
-GAUSSIAN_MEAN = 0.0
+GAUSSIAN_MEAN = 0.3
 GAUSSIAN_STDDEV = 0.1
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -45,8 +46,8 @@ class CustomDataset(Dataset):
         image.show()
 
 
-def process_with_visual_mixer(dataloader, rank_module, device):
-    save_path = "./shuffles/mixer/" + f"{IMAGE_BOX_W}x{IMAGE_BOX_H}x{VFE_BOX_SIZE}"
+def process_with_visual_mixer(dataloader, rank_module, device, vfe_box=VFE_BOX_SIZE):
+    save_path = "./shuffles/mixer/" + f"{IMAGE_BOX_W}x{IMAGE_BOX_H}x{vfe_box}"
     if os.path.exists(save_path):
         [os.remove(save_path + "/" + f) for f in os.listdir(save_path)]
     else:
@@ -59,16 +60,17 @@ def process_with_visual_mixer(dataloader, rank_module, device):
         inputs_batch = inputs_batch.to(device)
         _, channel_w, spatial_w = rank_module(inputs_batch)
         for j in range(inputs_batch.shape[0]):
-            v, _ = vfe(inputs_batch[j], VFE_BOX_SIZE, VFE_BOX_SIZE)
+            v, _ = vfe(inputs_batch[j], vfe_box, vfe_box)
             save_image(inputs_batch[j], save_path + f"/img_{i}a_{v:.2f}.png")
-            inputs_batch[j:, :, :] = rank(inputs_batch[j:, :, :], channel_w, spatial_w, VFE_BOX_SIZE, v, [0.5, 1])
-            v, _ = vfe(inputs_batch[j], VFE_BOX_SIZE, VFE_BOX_SIZE)
-            save_image(inputs_batch[j], save_path + f"/img_{i}b_{v:.2f}.png")
-        loop.set_description(f'Images: [{i + 1}/{len(dataloader)}]')
+            inputs_batch[j:, :, :] = rank(inputs_batch[j:, :, :], channel_w, spatial_w, vfe_box, v, [0.5, 1])
+            # v, _ = vfe(inputs_batch[j], vfe_box, vfe_box)
+            # save_image(inputs_batch[j], save_path + f"/img_{i}b_{v:.2f}.png")
+        loop.set_description(f'Images[vm]: [{i + 1}/{len(dataloader)}]')
+        print()
 
 
-def process_with_gaussian_noise(dataloader, mean=GAUSSIAN_MEAN, stddev=GAUSSIAN_STDDEV):
-    save_path = "./shuffles/gaussian/" + f"{IMAGE_BOX_W}x{IMAGE_BOX_H}x{VFE_BOX_SIZE}x[mean={mean},stddev={stddev}]"
+def process_with_gaussian_noise(dataloader, vfe_box=VFE_BOX_SIZE, mean=GAUSSIAN_MEAN, stddev=GAUSSIAN_STDDEV):
+    save_path = "./shuffles/gaussian/" + f"{IMAGE_BOX_W}x{IMAGE_BOX_H}x{vfe_box}x[mean={mean},stddev={stddev}]"
     if os.path.exists(save_path):
         [os.remove(save_path + "/" + f) for f in os.listdir(save_path)]
     else:
@@ -78,14 +80,23 @@ def process_with_gaussian_noise(dataloader, mean=GAUSSIAN_MEAN, stddev=GAUSSIAN_
     loop = tqdm(dataloader, total=(len(dataloader)))
     for i, (inputs_batch) in enumerate(loop):
         for j in range(inputs_batch.shape[0]):
-            v, _ = vfe(inputs_batch[j], VFE_BOX_SIZE, VFE_BOX_SIZE)
+            v, _ = vfe(inputs_batch[j], vfe_box, vfe_box)
             save_image(inputs_batch[j], save_path + f"/img_{i}a_{v:.2f}.png")
             noise = torch.randn(inputs_batch[j].size()) * stddev + mean
             n_image = inputs_batch[j] + noise
 
-            v, _ = vfe(n_image, VFE_BOX_SIZE, VFE_BOX_SIZE)
+            v, _ = vfe(n_image, vfe_box, vfe_box)
             save_image(n_image, save_path + f"/img_{i}b_{v:.2f}.png")
-        loop.set_description(f'Images: [{i + 1}/{len(dataloader)}]')
+        loop.set_description(f'Images[gn]: [{i + 1}/{len(dataloader)}]')
+        print()
+
+
+def process_with_gaussian_noise_wrap(args):
+    return process_with_gaussian_noise(*args)
+
+
+def process_with_visual_mixer_wrap(args):
+    return process_with_visual_mixer(*args)
 
 
 if __name__ == '__main__':
@@ -98,5 +109,16 @@ if __name__ == '__main__':
 
     c_dataset = CustomDataset(data_dir="./images", transform=custom_transforms)
     c_dataloader = DataLoader(c_dataset, batch_size=1, shuffle=False)
-    # process_with_visual_mixer(c_dataloader, rank_module, device)
-    process_with_gaussian_noise(c_dataloader)
+
+    with ProcessPoolExecutor(max_workers=8) as pool:
+        ft1 = pool.submit(process_with_visual_mixer_wrap, (c_dataloader, rank_module, device, 14))
+        # ft1 = pool.submit(process_with_visual_mixer_wrap, (c_dataloader, rank_module, device, 28))
+        # ft2 = pool.submit(process_with_gaussian_noise_wrap, (c_dataloader, 28, 0.0, 0.1))
+        # ft3 = pool.submit(process_with_gaussian_noise_wrap, (c_dataloader, 28, 0.3, 0.1))
+        # ft4 = pool.submit(process_with_gaussian_noise_wrap, (c_dataloader, 28, 0.5, 0.1))
+
+        # ft1 = pool.submit(process_with_gaussian_noise_wrap, (c_dataloader, 28, 0.0, 0.2))
+        # ft2 = pool.submit(process_with_gaussian_noise_wrap, (c_dataloader, 28, 0.0, 0.3))
+        # ft3 = pool.submit(process_with_gaussian_noise_wrap, (c_dataloader, 28, 0.0, 0.5))
+
+        wait([ft1])
